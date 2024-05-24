2ae20ba39b7f9069dd8114d3518629b313479cd3
Lolabird <89228316+Lolabird@users.noreply.github.com>
2024-05-23 20:40:49 -0600

    Move database files into datbase directory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./database');
+const db = require('./database/database');
 
 const app = express();
 const port = 3000;
